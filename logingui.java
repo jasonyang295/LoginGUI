a61f6060e5620bb0dc56d463a76c0a005bf1a2bc
Jason Yang <canyoncrestjy@gmail.com>
2021-05-25 14:24:41 -0700

    initalized frame and panel objects

---
+++ logingui.java
@@ -0,0 +1,10 @@
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+class logingui {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        JPanel panel = new JPanel();
+
+    }
+}
