439dbee7d8e24d8b3818489bc0ca5fb77cae696c
Jason Yang <canyoncrestjy@gmail.com>
2021-05-25 14:30:15 -0700

    more updates and added text to panel

--- logingui.java
+++ logingui.java
@@ -1,14 +1,28 @@
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-
+import javax.swing.JLabel;
+import javax.swing.JTextField;
 class logingui {
     public static void main(String[] args) {
         JPanel panel = new JPanel();
         JFrame frame = new JFrame();
         frame.setSize(100,100);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
+        
         frame.add(panel);
+
+        panel.setLayout(null);
+
+        JLabel label = new JLabel("User");
+        label.setBounds(10,20,80,25);
+        panel.add(label);
+        
+        JTextField userText = new JTextField();
+        userText.setBounds(100,20,165,25);
+        panel.add(userText);
+
+
+        frame.setVisible(true);
         
     }
 }
