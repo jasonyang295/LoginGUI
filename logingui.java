e465490f7ef6d3ec506def36bfb106dd6aedc8a1
Jason Yang <canyoncrestjy@gmail.com>
2021-05-25 14:33:24 -0700

    can clearly see user and password fields now

--- logingui.java
+++ logingui.java
@@ -2,24 +2,35 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
+import javax.swing.JPasswordField;
 class logingui {
     public static void main(String[] args) {
         JPanel panel = new JPanel();
         JFrame frame = new JFrame();
-        frame.setSize(100,100);
+        frame.setSize(350, 200); //updated bigger size
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         
         frame.add(panel);
 
         panel.setLayout(null);
 
-        JLabel label = new JLabel("User");
-        label.setBounds(10,20,80,25);
-        panel.add(label);
+        JLabel userlabel = new JLabel("User");
+        userlabel.setBounds(10,20,80,25);
+        panel.add(userlabel);
         
         JTextField userText = new JTextField();
         userText.setBounds(100,20,165,25);
         panel.add(userText);
+
+
+        JLabel passwordLabel = new JLabel("Password");
+        passwordLabel.setBounds(10,50,80,25);
+        panel.add(passwordLabel);
+
+        JPasswordField passwordText = new JPasswordField();
+        passwordText.setBounds(100,50,165, 25);
+        panel.add(passwordText);
+
 
 
         frame.setVisible(true);
